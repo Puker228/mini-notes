22a9b98890eb9381d2f596b8fec7a5ffa996958c
puker228 <pochta@pochta.com>
2026-05-09 22:12:22 +0300

    add graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"html/template"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"mini-notes/internal/notes"
 
@@ -13,6 +19,9 @@ import (
 var templateFS embed.FS
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	router := gin.Default()
 
 	t := template.Must(template.ParseFS(templateFS, "templates/*.html"))
@@ -25,5 +34,28 @@ func main() {
 	router.GET("/note/:id", h.GetNote)
 	router.POST("/note", h.CreateNote)
 
-	router.Run(":8000")
+	srv := &http.Server{
+		Addr:              ":8000",
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
